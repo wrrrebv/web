aa12d031147b6b3fe878e466268cab714a466dca
wrrrebv <156573629+wrrrebv@users.noreply.github.com>
2024-12-29 13:16:09 +0300

    добавили примеры запросов

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def ruwiki():
+    return render_template(R'ruwiki.html')
+
+@app.route('/hello')
+def hello():
+    return "Привет, мир!"
+    
+@app.route("/max")
+def find_max():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+
+    if a > b: 
+        return f"<h1>Максимум это число a: {a}</h1>"
+    else:
+        return f"<h1>Максимум это число b: {b}</h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
